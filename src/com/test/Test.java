78d954b800c099ddb3425d9ca8fea7507cab8afe
林 <@LAPTOP-3QM3H1IN>
2020-05-08 18:58:15 +0800

    123

--- src/com/test/Test.java
+++ src/com/test/Test.java
@@ -1,13 +1,7 @@
 package com.test;
 
 public class Test {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello World");
-		
-		
-		System.out.println("Hello World111");
 
 	}
 
